f113d1ff976378dd1d9d1474cb3a5abd7772be98
Matthew Yakubu <yakubumatthew76@gmail.com>
2025-04-22 19:30:56 -0400

    added a new test to test the init_database function within the resume script.

--- Code/test_text_filter.py
+++ Code/test_text_filter.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import MagicMock, patch
+
 from resume_parser_script import TextFilter
 # import nltk
 
@@ -15,7 +17,32 @@ class TestTextFilter(unittest.TestCase):
         expected_keyword = ['honest', 'today', 'good', 'day']
         self.assertEqual(extract_filter, expected_keyword)
 
+    @patch('resume_parser_script.psycopg2.connect')
+    @patch('resume_parser_script.os.getenv') # decorators that mock the psycopg2.connect function and the os.getenv call
+    # inside the resume_parser_script module.
 
+    # *order is reversed when passed into the function*
+    def test_init_database(self, mock_getenv, mock_connect):  # the function receives the mocked versions of "os.getenv"
+        # and "psycopg2.connect", respectively
+
+        mock_conn = MagicMock()  # simulates a database connection
+        mock_connect.return_value = mock_conn  # telling mock_connect() to return mock_conn
+
+        mock_getenv.return_value = "fake_password"  # mock get_env() will return 'fake password' when called with a key
+        result = TextFilter.init_database() # here we call the actual function
+
+        mock_getenv.assert_called_with("SECRET_PASSWORD") # check that the code tired to fetch the "SECRET_PASSWORD"
+        # from the environment
+
+        mock_connect.assert_called_with(
+            dbname = 'postgres',
+            user = 'matthewyakubu',
+            password = 'fake_password',
+            host = 'localhost',
+            port = '5432'
+        )  # checks that the psycopg2.connect call used the expected parameters.
+
+        self.assertEqual(result, mock_conn) # check that the method returns the mock connection
 
 if __name__ == '__main__':
     unittest.main()
